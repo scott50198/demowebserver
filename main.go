1f0099c59d7645eccd6195035c03dc52d2d4e914
root <root@instance-1.us-east1-b.c.scottserver-242114.internal>
2019-06-04 08:38:27 +0000

    Test git command

--- main.go
+++ main.go
@@ -20,6 +20,6 @@ func main() {
 	http.HandleFunc("/welcome", handler.WelcomeHandler)
 	http.Handle("/fronted/", http.StripPrefix("/fronted/", http.FileServer(http.Dir("fronted"))))
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":80", nil))
 
 }
